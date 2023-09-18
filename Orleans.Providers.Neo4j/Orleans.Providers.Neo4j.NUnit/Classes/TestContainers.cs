66027e040af2b3d183482bafd18f7b8859adea0b
licon4812 <jalickolli@gmail.com>
2023-09-19 08:37:20 +1000

    Created TestContainers in NUnit project

---
+++ Orleans.Providers.Neo4j/Orleans.Providers.Neo4j.NUnit/Classes/TestContainers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+namespace Orleans.Providers.Neo4j.NUnit.Classes
+{
+    public class TestContainers
+    {
+        private IContainer _container { get; set; }
+        public async void CreateNeo4jContainerAsync()
+        {
+            _container = new ContainerBuilder()
+                .WithImage("neo4j")
+                .WithExposedPort("7687")
+                .WithEnvironment("NEO4J_AUTH", "neo4j/strip-furnace-gages")
+                .WithEnvironment("NEO4J_dbms_memory_pagecache_size", "1G")
+                .WithEnvironment("NEO4J_dbms_memory_heap_max__size", "1G")
+                .WithEnvironment("NEO4J_dbms_memory_heap_initial__size", "1G")
+                .WithEnvironment("NEO4J_dbms_default__listen__address", "")
+                .Build();
+
+            // Start the container.
+            await _container.StartAsync()
+                .ConfigureAwait(false);
+
+        }
+        public async void DestroyNeo4jContainerAsync()
+        {
+            await _container.StopAsync();
+        }
+    }
+}
