bd4bdb1849a3ec9f8d595acc9607e756b126520e
Jeremy Carter <jeremychild@gmail.com>
2023-10-06 20:51:47 +1100

    Added INodeState.

---
+++ src/Orleans.Providers.Neo4j/Common/INodeState.cs
@@ -0,0 +1,9 @@
+﻿namespace Orleans.Providers.Neo4j.Common
+{
+    public interface INodeState
+    {
+        string ElementId { get; }
+        List<string> Labels { get; }
+        Dictionary<string, object> Properties { get; }
+    }
+}
